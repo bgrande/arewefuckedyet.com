124291ee066d371d2ec00d422f473b215f9f540e
Benedikt Grande <dev@bgrande.de>
2022-03-28 16:55:02 +0200

    fix(deploy): prevent dist from jekyll run as well

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@ sleep 1
 # copy file to target
 cp index.html doomsday.json dist/
 cp -r img dist/
+cp .nojekyll dist/
 
 # some adjustments if necessary
 sed -i ":a;N;\$!ba;s/$DEV_JSON_PATH/$JSON_PATH/g" dist/index.html
