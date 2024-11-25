a0b549f4899db968cc51c22fba3d7362acdadf78
Benedikt Grande <dev@bgrande.de>
2024-11-25 23:14:53 +0100

    fix: enable maybe-not

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ node clock.mjs
 sleep 1
 
 # copy file to target
-cp index.html doomsday.json impress.html authors-take.html dist/
+cp index.html doomsday.json impress.html authors-take.html maybe-not.html dist/
 cp -r img dist/
 cp .nojekyll dist/
 cp favicon.ico favicon.svg icon.png tile.png tile-wide.png dist/
@@ -21,6 +21,8 @@ cp sitemap.xml robots.txt site.webmanifest dist/
 
 # some adjustments if necessary
 sed -i ":a;N;\$!ba;s/$DEV_JSON_PATH/$JSON_PATH/g" dist/index.html
+sed -i ":a;N;\$!ba;s/$DEV_JSON_PATH/$JSON_PATH/g" dist/authors-take.html
+sed -i ":a;N;\$!ba;s/$DEV_JSON_PATH/$JSON_PATH/g" dist/maybe-not.html
 #sed -i ":a;N;\$!ba;s/$DEV_LOG_PATH/$LOG_PATH/g" dist/index.html
 #cp -r service dist/
 #rm dist/service/composer*
