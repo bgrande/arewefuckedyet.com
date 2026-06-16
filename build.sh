2e6693e09bbc419fe80e1971a56e9a7a06d17557
Benedikt Grande <dev@bgrande.de>
2026-06-16 18:28:48 +0200

    chore: separate build and deployment so we can skip the deployment if build failed

--- build.sh
+++ build.sh
@@ -9,23 +9,6 @@ sleep 1
 NODE_ENV=$APP_ENV npx tailwindcss --no-autoprefixer -o ./dist/tailwind.css --minify
 
 # create doomsday clock json
-node clock.mjs
-sleep 1
+node clock.mjs && echo 'Build finished!' && sleep 1 && exit 0
 
-# copy file to target
-cp index.html doomsday.json impress.html authors-take.html maybe-not.html dist/
-cp -r img dist/
-cp .nojekyll dist/
-cp favicon.ico favicon.svg icon.png tile.png tile-wide.png dist/
-cp sitemap.xml robots.txt site.webmanifest dist/
-
-# some adjustments if necessary
-sed -i ":a;N;\$!ba;s/$DEV_JSON_PATH/$JSON_PATH/g" dist/index.html
-sed -i ":a;N;\$!ba;s/$DEV_JSON_PATH/$JSON_PATH/g" dist/authors-take.html
-sed -i ":a;N;\$!ba;s/$DEV_JSON_PATH/$JSON_PATH/g" dist/maybe-not.html
-#sed -i ":a;N;\$!ba;s/$DEV_LOG_PATH/$LOG_PATH/g" dist/index.html
-#cp -r service dist/
-#rm dist/service/composer*
-#rm -rf dist/service/data/usage/*
-#cp -r img dist/
-#cp -r vid dist/
+exit 1
---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# copy file to target
+cp index.html doomsday.json impress.html authors-take.html maybe-not.html dist/
+cp -r img dist/
+cp .nojekyll dist/
+cp favicon.ico favicon.svg icon.png tile.png tile-wide.png dist/
+cp sitemap.xml robots.txt site.webmanifest dist/
+
+# some adjustments if necessary
+sed -i ":a;N;\$!ba;s/$DEV_JSON_PATH/$JSON_PATH/g" dist/index.html
+sed -i ":a;N;\$!ba;s/$DEV_JSON_PATH/$JSON_PATH/g" dist/authors-take.html
+sed -i ":a;N;\$!ba;s/$DEV_JSON_PATH/$JSON_PATH/g" dist/maybe-not.html
+#sed -i ":a;N;\$!ba;s/$DEV_LOG_PATH/$LOG_PATH/g" dist/index.html
+#cp -r service dist/
+#rm dist/service/composer*
+#rm -rf dist/service/data/usage/*
+#cp -r img dist/
+#cp -r vid dist/
+
+ echo Deploy finished!
+ echo Done!
